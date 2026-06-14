4312ae763ff1aa1648a2b65d218de401575b08e6
Joao-VitorGD1703 <douvernyj@gmail>
2026-06-14 14:57:26 -0300

    log

--- admin/src/main.js
+++ admin/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-
+console.log("ola")
 const app = createApp(App)
 const pinia = createPinia()
 
